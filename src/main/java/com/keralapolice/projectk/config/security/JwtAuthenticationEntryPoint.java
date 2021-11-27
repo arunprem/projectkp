a65f0fea2f8634c2f4c8edd8975fa6b254a76a70
DESKTOP-GDT0I9S\Arunprem <kuttunick@gmail.com>
2021-11-27 18:24:35 +0530

    security

---
+++ src/main/java/com/keralapolice/projectk/config/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.keralapolice.projectk.config.security;
+
+import com.google.gson.Gson;
+import com.keralapolice.projectk.config.exception.InvalidLoginResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(loginResponse);
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setStatus(401);
+        httpServletResponse.getWriter().print(jsonLoginResponse);
+    }
+}
---
+++ src/main/java/com/keralapolice/projectk/config/security/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.keralapolice.projectk.config.security;
+
+public class SecurityConstants {
+    public static final String  SIGN_UP_URLS="/api/user/**";
+    public static final String SECRET_KEY="SecrectKeyToGenerateJwtToken";
+    public static final String TOKEN_PREFIX="Bearer ";
+
+}
